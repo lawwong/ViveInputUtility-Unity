85ae454753dcb2029cc838013f6dff9ece8a80bc
lawwong <lawrence_wong@htc.com>
2017-12-10 00:42:24 +0800

    Refine VIUSettings preference window content

--- Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/VIUSettings.cs
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/VIUSettings.cs
@@ -12,8 +12,8 @@ namespace HTC.UnityPlugin.Vive
         public const string BIND_UI_SWITCH_TOOLTIP = "When enabled, pressing RightShift + B to open the binding interface in play mode.";
         public const string EX_CAM_UI_SWITCH_TOOLTIP = "When enabled, pressing RightShift + M to toggle the quad view while external camera config file exist.";
         public const string SIMULATOR_PAD_TOUCH_SWITCH_TOOLTIP = "Use Shift key to lock rotation and simulate pad touch";
-        public const string SIMULATOR_KEY_MOVE_SPEED_TOOLTIP = "meter per second";
-        public const string SIMULATOR_KEY_ROTATE_SPEED_TOOLTIP = "angle per second";
+        public const string SIMULATOR_KEY_MOVE_SPEED_TOOLTIP = "W/A/S/D";
+        public const string SIMULATOR_KEY_ROTATE_SPEED_TOOLTIP = "Arrow Up/Down/Left/Right";
 
         public const bool ENABLE_BINDING_INTERFACE_SWITCH_DEFAULT_VALUE = false;
         public const bool ENABLE_EXTERNAL_CAMERA_SWITCH_DEFAULT_VALUE = false;
@@ -127,6 +127,7 @@ namespace HTC.UnityPlugin.Vive
             if (s_instance == this)
             {
                 s_instance = null;
+                s_loadedAssetPath = null;
             }
         }
 
@@ -177,6 +178,7 @@ namespace HTC.UnityPlugin.Vive
             if (settings == null)
             {
                 UnityEditor.AssetDatabase.CreateAsset(s_instance, path);
+                s_loadedAssetPath = path;
             }
             else
             {
@@ -189,12 +191,15 @@ namespace HTC.UnityPlugin.Vive
             if (isLoaded && !string.IsNullOrEmpty(loadedAssetPath))
             {
                 UnityEditor.AssetDatabase.DeleteAsset(loadedAssetPath);
+
+                s_instance = null;
+                s_loadedAssetPath = null;
             }
         }
 
         private static GUIStyle s_enabledLabel;
         private static GUIStyle s_disabledLabel;
-
+        private const float INDENT_PIXEL = 20f;
         [UnityEditor.PreferenceItem("VIU Settings")]
         private static void OnVIUPreferenceGUI()
         {
@@ -207,33 +212,27 @@ namespace HTC.UnityPlugin.Vive
                 s_disabledLabel.normal.textColor = Color.gray;
             }
 
-            if (GUILayout.Button("Reset to Default Settings"))
+            UnityEditor.EditorGUI.BeginChangeCheck();
+
+            enableBindingInterfaceSwitch = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Enable Binding Interface Switch", BIND_UI_SWITCH_TOOLTIP), enableBindingInterfaceSwitch);
+            enableExternalCameraSwitch = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Enable External Camera Switch", EX_CAM_UI_SWITCH_TOOLTIP), enableExternalCameraSwitch);
+
+            // simulator
+            simulatorSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Simulator Module Support"), simulatorSupport, s_enabledLabel);
+            if (simulatorSupport)
             {
-                EditorRemoveLoadedAsset();
+                UnityEditor.EditorGUI.indentLevel++;
+
+                simulatorAutoTrackMainCamera = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Auto Main Camera Tracking"), simulatorAutoTrackMainCamera);
+                simulatorPadTouchSwitch = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Simulate Trackpad Touch", SIMULATOR_PAD_TOUCH_SWITCH_TOOLTIP), simulatorPadTouchSwitch);
+                simulatorKeyMoveSpeed = UnityEditor.EditorGUILayout.DelayedFloatField(new GUIContent("Keyboard Move Speed", SIMULATOR_KEY_MOVE_SPEED_TOOLTIP), simulatorKeyMoveSpeed);
+                simulatorKeyRotateSpeed = UnityEditor.EditorGUILayout.DelayedFloatField(new GUIContent("Keyboard Rotate Speed", SIMULATOR_KEY_ROTATE_SPEED_TOOLTIP), simulatorKeyRotateSpeed);
+                simulatorMouseRotateSpeed = UnityEditor.EditorGUILayout.DelayedFloatField(new GUIContent("Mouse Rotate Speed"), simulatorMouseRotateSpeed);
+
+                UnityEditor.EditorGUI.indentLevel--;
             }
-            else
-            {
-                UnityEditor.EditorGUI.BeginChangeCheck();
 
-                enableBindingInterfaceSwitch = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Enable Binding Interface Switch", BIND_UI_SWITCH_TOOLTIP), enableBindingInterfaceSwitch);
-                enableExternalCameraSwitch = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Enable External Camera Switch", EX_CAM_UI_SWITCH_TOOLTIP), enableExternalCameraSwitch);
-
-                // simulator
-                simulatorSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Simulator Module Support"), simulatorSupport, s_enabledLabel);
-                if (simulatorSupport)
-                {
-                    UnityEditor.EditorGUI.indentLevel++;
-
-                    simulatorAutoTrackMainCamera = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Auto Main Camera Tracking"), simulatorAutoTrackMainCamera);
-                    simulatorPadTouchSwitch = UnityEditor.EditorGUILayout.Toggle(new GUIContent("Pad Touch Switch", SIMULATOR_PAD_TOUCH_SWITCH_TOOLTIP), simulatorPadTouchSwitch);
-                    simulatorKeyMoveSpeed = UnityEditor.EditorGUILayout.DelayedFloatField(new GUIContent("Key Move Speed", SIMULATOR_KEY_MOVE_SPEED_TOOLTIP), simulatorKeyMoveSpeed);
-                    simulatorKeyRotateSpeed = UnityEditor.EditorGUILayout.DelayedFloatField(new GUIContent("Key Rotate Speed", SIMULATOR_KEY_ROTATE_SPEED_TOOLTIP), simulatorKeyRotateSpeed);
-                    simulatorMouseRotateSpeed = UnityEditor.EditorGUILayout.DelayedFloatField(new GUIContent("Mouse Move Speed"), simulatorMouseRotateSpeed);
-
-                    UnityEditor.EditorGUI.indentLevel--;
-                }
-
-                // native
+            // native
 #if UNITY_5_5_OR_NEWER
                 unityNativeVRSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Unity Native VR Module Support"), unityNativeVRSupport, s_enabledLabel);
                 if (unityNativeVRSupport)
@@ -247,47 +246,65 @@ namespace HTC.UnityPlugin.Vive
                     UnityEditor.EditorGUI.indentLevel--;
                 }
 #else
-                UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Unity Native VR Module Support"), false, s_disabledLabel);
-                UnityEditor.EditorGUI.indentLevel++;
-
-                if (GUILayout.Button("Get Unity 5.5 or later version"))
-                {
-                    Application.OpenURL("https://unity3d.com/get-unity/download");
-                }
-                UnityEditor.EditorGUI.indentLevel--;
-#endif
-
-                // openvr
-#if VIU_STEAMVR
-                steamVRSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Steam VR Module Support"), steamVRSupport, s_enabledLabel);
-#else
-                UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Steam VR Module Support"), false, s_disabledLabel);
-                UnityEditor.EditorGUI.indentLevel++;
-                if (GUILayout.Button("Get SteamVR Plugin"))
-                {
-                    Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/content/32647");
-                }
-                UnityEditor.EditorGUI.indentLevel--;
-#endif
-
-                // oculuas
-#if VIU_OCULUSVR
-                oculusVRSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Oculus VR Module Support"), oculusVRSupport, s_enabledLabel);
-#else
-                UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Oculus VR Module Support"), false, s_disabledLabel);
-                UnityEditor.EditorGUI.indentLevel++;
-                if (GUILayout.Button("Get Oculus VR Plugin"))
-                {
-                    Application.OpenURL("https://developer.oculus.com/downloads/package/oculus-utilities-for-unity-5/");
-                }
-                UnityEditor.EditorGUI.indentLevel--;
-#endif
-
-                if (UnityEditor.EditorGUI.EndChangeCheck())
-                {
-                    EditorSave();
-                }
+            GUI.enabled = false;
+            UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Unity Native VR Module Support"), false, s_enabledLabel);
+            GUI.enabled = true;
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(INDENT_PIXEL);
+            if (GUILayout.Button("Get Unity 5.5 or later version"))
+            {
+                Application.OpenURL("https://unity3d.com/get-unity/download");
             }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+#endif
+
+            // openvr
+#if VIU_STEAMVR
+            steamVRSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Steam VR Module Support"), steamVRSupport, s_enabledLabel);
+#else
+            GUI.enabled = false;
+            UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Steam VR Module Support"), false, s_enabledLabel);
+            GUI.enabled = true;
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(INDENT_PIXEL);
+            if (GUILayout.Button("Get SteamVR Plugin"))
+            {
+                Application.OpenURL("https://www.assetstore.unity3d.com/en/#!/content/32647");
+            }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+#endif
+
+            // oculuas
+#if VIU_OCULUSVR
+            oculusVRSupport = UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Oculus VR Module Support"), oculusVRSupport, s_enabledLabel);
+#else
+            GUI.enabled = false;
+            UnityEditor.EditorGUILayout.ToggleLeft(new GUIContent("Oculus VR Module Support"), false, s_enabledLabel);
+            GUI.enabled = true;
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(INDENT_PIXEL);
+            if (GUILayout.Button("Get Oculus VR Plugin"))
+            {
+                Application.OpenURL("https://developer.oculus.com/downloads/package/oculus-utilities-for-unity-5/");
+            }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+#endif
+
+            if (UnityEditor.EditorGUI.EndChangeCheck())
+            {
+                EditorSave();
+            }
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Reset"))
+            {
+                EditorRemoveLoadedAsset();
+            }
+            GUILayout.EndHorizontal();
         }
 #endif
     }
